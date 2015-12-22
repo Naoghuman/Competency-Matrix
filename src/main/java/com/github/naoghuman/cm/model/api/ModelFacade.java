946679c0365696c2631b61ca8f4949d1af8aaeb7
PRo <peter.rogge@yahoo.de>
2015-12-23 00:46:45 +0100

    Basic implemenation for the models.

---
+++ src/main/java/com/github/naoghuman/cm/model/api/ModelFacade.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2015 PRo
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.github.naoghuman.cm.model.api;
+
+import com.github.naoghuman.cm.model.Category;
+import com.github.naoghuman.cm.model.Level;
+import com.github.naoghuman.cm.model.Matrix;
+import com.github.naoghuman.cm.model.SubCategory;
+
+/**
+ *
+ * @author PRo
+ */
+public enum ModelFacade {
+    
+    INSTANCE;
+    
+    public ICategory getDefaultCategory() {
+        return new Category();
+    }
+    
+    public ILevel getDefaultLevel() {
+        return new Level();
+    }
+    
+    public IMatrix getDefaultMatrix() {
+        return new Matrix();
+    }
+    
+    public ISubCategory getDefaultSubCategory() {
+        return new SubCategory();
+    }
+    
+}
