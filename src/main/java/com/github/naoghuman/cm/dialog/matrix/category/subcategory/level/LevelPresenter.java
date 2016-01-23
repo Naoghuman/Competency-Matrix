d0c43c58e9f345e804b7aa71442dc64c9ec0f6c6
PRo <peter.rogge@yahoo.de>
2016-01-23 19:50:49 +0100

    Refactore the packages from Level (for dialog).

--- src/main/java/com/github/naoghuman/cm/dialog/matrix/category/subcategory/level/LevelPresenter.java
+++ src/main/java/com/github/naoghuman/cm/dialog/matrix/category/subcategory/level/LevelPresenter.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package com.github.naoghuman.cm.matrix.category.subcategory.level;
+package com.github.naoghuman.cm.dialog.matrix.category.subcategory.level;
 
 import com.github.naoghuman.cm.configuration.api.IActionConfiguration;
 import com.github.naoghuman.cm.configuration.api.IRegisterActions;
--- src/main/java/com/github/naoghuman/cm/dialog/matrix/category/subcategory/level/LevelView.java
+++ src/main/java/com/github/naoghuman/cm/dialog/matrix/category/subcategory/level/LevelView.java
@@ -14,8 +14,9 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package com.github.naoghuman.cm.matrix.category.subcategory.level;
+package com.github.naoghuman.cm.dialog.matrix.category.subcategory.level;
 
+import com.github.naoghuman.cm.dialog.matrix.category.subcategory.level.LevelPresenter;
 import com.airhacks.afterburner.views.FXMLView;
 
 /**
